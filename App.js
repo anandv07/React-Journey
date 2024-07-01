de91661ef0ce063e078133571a46c114cc6e90c9
Anand <anandv072001@gmail.com>
2024-07-01 21:50:24 +0530

    react journey starts

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+    "div",
+    {id:"heading",xyz:"123"},
+    "Hello World from React!"
+    );
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [React.createElement("h1",{id:"head1"},"inside heading 1"),
+    React.createElement("h2",{id:"head2"},"inside heading 2")]);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
